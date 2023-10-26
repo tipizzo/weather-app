cf1b7aba9ea3de3a4d7fee0628ea6a2292f826fd
KABANGA David <Davkabanga96@gmail.com>
2023-10-26 16:12:22 +0200

    Setting up the API to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import loader from "./assets/loader.svg"
+import { useEffect, useState } from "react";
+import loader from "./assets/loader.svg";
 import "./App.css";
 
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
+
 function App() {
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(responseData => {
+      console.log(responseData);
+      setWeatherData({
+        city: responseData.data.city,
+        country: responseData.data.country,
+        iconId: responseData.data.current.weather.ic,
+        temperature: responseData.data.current.weather.tp,
+      })
+    })
+  }, [])
  
   return (
 
       <main>
-          <div className="loader-container">
-              <img src={loader} />
+          <div className={`loader-container ${!weatherData && "active"}`}>
+              <img src={loader} allt="loading icon" />
           </div>
-          <p className="city-name">Paris</p>
-          <p className="country-name">France</p>
-          <p className="temperature">17°</p>
-          <div className="info-icon-container">
-              <img src="/icons/01d.svg" className="info-icon" alt="weather icon"/>
-          </div>
+          
+          {weatherData && (
+            <>
+              <p className="city-name">{weatherData.city}</p>
+              <p className="country-name">{weatherData.country}</p>
+              <p className="temperature">{weatherData.temperature}°</p>
+              <div className="info-icon-container">
+                <img src={`/icons/${weatherData.iconId}.svg`} className="info-icon" alt="weather icon"/>
+              </div>
+            </>
+
+          )}
+
+          
       </main>
   
   );
